1b83ccfa74532f76512117a7baa9d4092166de8c
thenriqw <thalisonhenriqw@gmail.com>
2026-08-28 21:55:10 -0300

    feat: add script to compress build directory
    Add the `archiver` dependency and a new script `zip_dist.cjs` to automate the creation of a distributable ZIP file from the `dist` folder.

---
+++ zip_dist.cjs
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+const zlib = require('zlib');
+const archiver = require('archiver');
+
+const output = fs.createWriteStream(__dirname + '/efederal-dist.zip');
+const archive = archiver('zip', {
+  zlib: { level: 9 } // Sets the compression level.
+});
+
+output.on('close', function() {
+  console.log(archive.pointer() + ' total bytes');
+  console.log('archiver has been finalized and the output file descriptor has closed.');
+});
+
+archive.on('error', function(err) {
+  throw err;
+});
+
+archive.pipe(output);
+archive.directory('dist/', 'dist');
+archive.finalize();
